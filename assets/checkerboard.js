e1585d83e149969a00be37a3f7619990bca8cd21
Joshua Taylor <JoshuaRTaylor87@gmail.com>
2022-11-18 14:36:52 -0500

    color pattern functionality

--- assets/checkerboard.js
+++ assets/checkerboard.js
@@ -1,19 +1,46 @@
-// use numberToSwap = 2 for everyother and numberToSwap = 3 for checkerboard
-function checkerboardOrSwap(numberToSwap) {
-  let cards = document.querySelectorAll(".checkerboard-product");
-  cards.forEach((element, index) => {
+/*
+
+option 1 - alt (tan first) desktop, checker (tan first) mobile
+
+option 2 - checker (grey first) desktop, checker (tan first) mobile
+
+option 3 - checker (grey first) desktop, alt (grey first) mobile
+
+*/
+
+let pattern_option_1 = document.querySelectorAll(".color-pattern-opt-1");
+let pattern_option_2 = document.querySelectorAll(".color-pattern-opt-2");
+let pattern_option_3 = document.querySelectorAll(".color-pattern-opt-3");
+
+function gridColors(numberToSwap, elementArray, tan_first) {
+  elementArray.forEach((element, index) => {
     let swapNumber = index % numberToSwap;
-    if (swapNumber == 0) {
-      let images = element.querySelectorAll("img");
-      images.forEach(element => element.classList.toggle("hidden"))
+    let images = element.querySelectorAll("img");
+
+    if (tan_first) {
+      images.forEach(element => element.classList.toggle("hidden"));
     }
+    if (swapNumber == 0) {
+      images.forEach((element) => {
+        element.classList.toggle("hidden");
+      });
+    };
   });
+  console.log("function ran");
 }
 
+
+// use numberToSwap = 2 for everyother and numberToSwap = 3 for checkerboard
 window.addEventListener("load", () => {
   if (window.innerWidth < 750) {
-    checkerboardOrSwap(3);
+    gridColors(3, pattern_option_1, true);
+    gridColors(3, pattern_option_2, true);
+    gridColors(2, pattern_option_3, false);
+    console.log("mobile");
   } else {
-    checkerboardOrSwap(2);
+    gridColors(2, pattern_option_1, true);
+    gridColors(3, pattern_option_2, false);
+    gridColors(3, pattern_option_3, false);
+    console.log(alternate_pattern);
   }
 });
